5909dae5b86bf18bdfa2c5331617866ea325c335
Snabeldier <79211348+Snabeldier@users.noreply.github.com>
2021-12-24 16:57:01 +0100

    Changed Fraction to FractionService

--- src/main/java/minevalley/vince/template/module/commands/ExampleCommand.java
+++ src/main/java/minevalley/vince/template/module/commands/ExampleCommand.java
@@ -1,19 +1,19 @@
 package minevalley.vince.template.module.commands;
 
 import minevalley.core.api.User;
-import minevalley.core.api.enums.Fraction;
+import minevalley.core.api.enums.FractionService;
 import minevalley.core.api.enums.TeamRank;
-import minevalley.core.api.utils.command.PlayerCommand;
 import minevalley.core.api.utils.command.Command;
 import minevalley.core.api.utils.command.CommandOptions;
+import minevalley.core.api.utils.command.PlayerCommand;
 
 import java.util.List;
 
 @CommandOptions(
-        commands = {"example", "ex"}, // command-spelling and aliases
+        commands = {"example", "ex"}, // command-spelling and aliases (Always have to be written in lower case!)
         successMessage = "", // message to print, if onCommand() returns SUCCESS
         playerRank = {}, // this can be omitted (default value is {})
-        fractions = {Fraction.POLICE},
+        fractions = {FractionService.POLICE},
         teamRanks = {TeamRank.DEVELOPMENT, TeamRank.ADMINISTRATION, TeamRank.SERVER_LEAD},
         requireSupportService = false, // user has to be in support service (if false, option can be omitted)
         syntax = "example <necessary argument> [<unnecessary argument>]", //correct syntax
