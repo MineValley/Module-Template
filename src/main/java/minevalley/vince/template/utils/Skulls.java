c2b9fd1759d82749c1a7f19708db881b50d8a588
Snabeldier <79211348+Snabeldier@users.noreply.github.com>
2022-02-28 06:05:09 +0100

    Added useful tip

--- src/main/java/minevalley/vince/template/utils/Skulls.java
+++ src/main/java/minevalley/vince/template/utils/Skulls.java
@@ -1,13 +1,19 @@
 package minevalley.vince.template.utils;
 
-import minevalley.core.api.Core;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import minevalley.core.api.Core;
 import org.bukkit.inventory.ItemStack;
 
 @AllArgsConstructor
 @Getter
 public enum Skulls {
+
+    /**
+     * Use classes/enums like this to cache often-used itemstacks or so, to reduce lacks of performance
+     * <b>Note:</b> Using enums for skull classes is recommended but not required.
+     * A usual class with static final object may do their job just as an enum does. Both is accepted as good style.
+     */
 
     EXAMPLE_SKULL_1(Core.createItem("SKULL-URL1").build()),
     EXAMPLE_SKULL_2(Core.createItem("SKULL-URL2").setDisplayName("NAME").build()),
