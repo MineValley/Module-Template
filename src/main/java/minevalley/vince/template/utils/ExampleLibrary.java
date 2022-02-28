019547b1b4123650045f0f756a3548c08c1e435c
Snabeldier <79211348+Snabeldier@users.noreply.github.com>
2022-02-28 06:02:00 +0100

    Made ExampleLibrary final

--- src/main/java/minevalley/vince/template/utils/ExampleLibrary.java
+++ src/main/java/minevalley/vince/template/utils/ExampleLibrary.java
@@ -1,6 +1,6 @@
 package minevalley.vince.template.utils;
 
-public class ExampleLibrary {
+public final class ExampleLibrary {
 
     public static void exampleMethod(String string, int integer) {
         // Logic here
